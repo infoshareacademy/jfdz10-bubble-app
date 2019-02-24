8bfa164067819ef26477dc5234bc8ba80135f954
mik5owski <piatkowskimikolaj@gmail.com>
2019-02-24 19:14:01 +0100

    initial commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navigation from '../navigation/Navigation'
 
 class App extends Component {
   render() {
@@ -20,6 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Navigation />
       </div>
     );
   }
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React, { Component } from  'react'
+
+class Navigation extends Component {
+    render() {
+        return (
+        <div className="Navigation">
+            <ul className="ListContainer">
+                <li><h1>Hello</h1></li>
+            </ul>
+        </div>
+        )
+    }
+}
+
+export default Navigation
