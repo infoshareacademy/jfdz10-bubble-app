6d64e260a60a1c117e89dd0d9606de2caa819036
mik5owski <piatkowskimikolaj@gmail.com>
2019-02-24 20:43:09 +0100

    add basic structure for navigation

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,15 +1,21 @@
-import React, { Component, Router } from  'react'
+import React, { Component } from  'react'
+import {Route, NavLink, Router} from 'react-router-dom'
+
+// import Players from '../Players'
+// import Matches from '../Matches'
+// import Profile from '../Profile'
+// import AddMatch from '../AddMatch'
+
+import Logout from './Logout'
+import Profile from './Profile'
 
 class Navigation extends Component {
     render() {
         return (
         <div className="Navigation">
             <ul className="ListContainer">
-                <li>Hello</li>
-                <li>there.</li>
-                <li>How</li>
-                <li>are</li>
-                <li>you?</li>
+                <li><NavLink to="/logout">Logout</NavLink></li>
+                <li><NavLink to="/profile">Profile</NavLink></li>
             </ul>
         </div>
         )
