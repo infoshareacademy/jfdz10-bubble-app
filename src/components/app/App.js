90c5525d7c05a68514fe5a71a881b6e050712700
crchs <kolenycz@gmail.com>
2019-02-24 20:39:08 +0100

    added players table

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Players from '../players/Players'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Players />
       </div>
     );
   }
--- src/components/players/Players.js
+++ src/components/players/Players.js
@@ -1,37 +1,75 @@
-import React from 'react'
-import { Button, Image, List } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Header, Image, Table, Button, Icon } from 'semantic-ui-react'
 
-const ListExampleFloated = () => (
-  <List divided verticalAlign='middle'>
-    <List.Item>
-      <List.Content floated='right'>
-        <Button>Add</Button>
-      </List.Content>
-      <Image avatar src='/images/avatar/small/lena.png' />
-      <List.Content>Lena</List.Content>
-    </List.Item>
-    <List.Item>
-      <List.Content floated='right'>
-        <Button>Add</Button>
-      </List.Content>
-      <Image avatar src='/images/avatar/small/lindsay.png' />
-      <List.Content>Lindsay</List.Content>
-    </List.Item>
-    <List.Item>
-      <List.Content floated='right'>
-        <Button>Add</Button>
-      </List.Content>
-      <Image avatar src='/images/avatar/small/mark.png' />
-      <List.Content>Mark</List.Content>
-    </List.Item>
-    <List.Item>
-      <List.Content floated='right'>
-        <Button>Add</Button>
-      </List.Content>
-      <Image avatar src='/images/avatar/small/molly.png' />
-      <List.Content>Molly</List.Content>
-    </List.Item>
-  </List>
-)
+// import Player from './Player'
 
-export default ListExampleFloated
+
+
+const fetchPlayers = () =>
+    fetch(process.env.PUBLIC_URL + "/players.json").then(response =>
+        response.json()
+    );
+
+
+
+class Players extends Component {
+
+    state = {
+        players: []
+    };
+
+    componentDidMount() {
+        fetchPlayers().then(players => this.setState({ players }));
+    }
+
+
+    render() {
+        
+        return (
+
+            <Table basic='very' celled collapsing>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Player</Table.HeaderCell>
+                        <Table.HeaderCell>Location</Table.HeaderCell>
+                        <Table.HeaderCell>Sports</Table.HeaderCell>
+                        <Table.HeaderCell>Add to Favorites</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    {this.state.players.map(
+                        player => (
+                            <Table.Row>
+
+                                <Table.Cell>
+                                    <Header as='h4' image>
+                                        <Image src={player.avatar} rounded size='mini' />
+                                        <Header.Content>
+                                            {player.name}
+                                            <Header.Subheader>{player.eMail}</Header.Subheader>
+                                        </Header.Content>
+                                    </Header>
+                                </Table.Cell>
+
+                                <Table.Cell>{player.localization}</Table.Cell>
+
+                                <Table.Cell>{player.favouriteSportsIDs}</Table.Cell>
+
+                                <Table.Cell>
+                                    <Button icon>
+                                        <Icon name='favorite' />
+                                    </Button>
+                                </Table.Cell>
+
+                            </Table.Row>
+                        )
+                    )}
+
+                </Table.Body>
+            </Table>
+        )
+    }
+}
+
+export default Players
