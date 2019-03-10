2d580f49340d8d85f722256e8ec45044d14d4d0b
WitoG <wit1godlewski@gmail.com>
2019-03-10 20:51:35 +0100

    added grid scheme to Sport-details

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import SportDetails from '../Sport-details/Sport-details';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <SportDetails />
       </div>
     );
   }
