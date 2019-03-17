2a243aa9a815783dd8db29110f019cd0b3aa5b57
WitoG <wit1godlewski@gmail.com>
2019-03-17 20:44:05 +0100

    -

--- src/components/Sport-details/Sport-details.js
+++ src/components/Sport-details/Sport-details.js
@@ -1,33 +1,51 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+//import ReactDOM from "react-dom";
 // import { ReactComponent } from '*.svg';
-import { Grid, Image, Segment } from 'semantic-ui-react'
+import { Grid, Segment } from "semantic-ui-react";
+import { List } from "semantic-ui-react";
 
-class SportDetails extends Component{
-    render() {return (
-  <Grid stackable columns={2}>
-    <Grid.Column>
-      <Segment>
-        <p>lorem ipsum</p>
-      </Segment>
-    </Grid.Column>
-    <Grid.Column>
-      <Segment>
-        <p>lorem</p>
-      </Segment>
-    </Grid.Column>
-    <Grid.Column>
-      <Segment>
-        <p>lorem</p>
-      </Segment>
-    </Grid.Column>
-    <Grid.Column>
-      <Segment>
-        <p>lorem</p>
-      </Segment>
-    </Grid.Column>
-  </Grid>
-)
-    }
+class SportDetails extends Component {
+  render() {
+    return (
+      <Grid stackable columns={2}>
+        <Grid.Column>
+          <Segment>
+            <List>
+              <List.Item>
+                <List.Header>Sport</List.Header>A lovely city
+              </List.Item>
+              <List.Item>
+                <List.Header>Adress</List.Header>
+                Also quite a lovely city
+              </List.Item>
+              <List.Item>
+                <List.Header>Date of event</List.Header>
+                Sometimes can be a lovely city
+              </List.Item>
+              <List.Item>
+                <List.Header>Time of event</List.Header>
+                What a lovely city
+              </List.Item>
+            </List>
+          </Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>
+            <p>MAPA</p>
+          </Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>
+            <p>lorem</p>
+          </Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>
+            <p>Komentarz hosta</p>
+          </Segment>
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 export default SportDetails;
