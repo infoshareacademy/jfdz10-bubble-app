8a8641b309a609b604a4c19de628f73a0ab034c4
mik5owski <piatkowskimikolaj@gmail.com>
2019-03-21 12:19:07 +0100

    add basic structure to Player component

---
+++ src/components/player/Player.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './Player.css'
+
+class Player extends Component {
+    state = {
+
+    }
+
+    render() {
+        return(
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
+
+export default Player
