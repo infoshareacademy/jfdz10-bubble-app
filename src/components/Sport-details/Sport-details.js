93eb675ae9142476fbd4d529bbd4abfe46c6857b
WitoG <wit1godlewski@gmail.com>
2019-03-23 10:19:23 +0100

    -

--- src/components/Sport-details/Sport-details.js
+++ src/components/Sport-details/Sport-details.js
@@ -1,115 +1,71 @@
 import React, { Component } from "react";
-// import ReactDOM from "react-dom";
-// import { ReactComponent } from '*.svg';
 import { Grid, Segment } from "semantic-ui-react";
 import { List } from "semantic-ui-react";
 
-
-const fetchPlayers = async () => {
-  const response = await fetch(process.env.PUBLIC_URL + "/players.json");
-  const players = await response.json();
-  return players;
-}
-
-const fetchUser = async () => {
-  const response = await fetch(process.env.PUBLIC_URL + '/user.json')
-  const user = await response.json()
-  return user
-}
-
-const fetchSports = async () => {
-  const response = await fetch(process.env.PUBLIC_URL + "/sports.json");
-  const sports = await response.json();
-  return sports;
-}
-
-const fetchMatches = async () => {
-  const response = await fetch(process.env.PUBLIC_URL + "/matches.json");
-  const matches = await response.json();
-  return matches;
-}
-
 class SportDetails extends Component {
-
-  state = {
-    players: [],
-    user: [],
-    sports: [],
-    matches: []
-
-  }
-
-  componentDidMount() {
-    Promise.all([fetchUser(), fetchSports(), fetchPlayers(), fetchMatches()])
-      .then(([user, sports, players, matches]) => this.setState({
-        user: user,
-        sports: sports,
-        players: players,
-        matches: matches
-      })
-      )
-  }
 
   //AddPlayerToEvent = () => { }
 
   render() {
 
     return (
-      <Grid stackable columns={2}>
-        <Grid.Column>
-          <Segment>
-            <List>
-              <List.Item>
-                <List.Header>Sport</List.Header>{}
-              </List.Item>
-              <List.Item>
-                <List.Header>Adress</List.Header>
-                {this.state.matches.map((match) => <li key={match.localization.street}>{match.localization.city}</li>)}
-              </List.Item>
-              <List.Item>
-                <List.Header>Date of event</List.Header>
-                {this.state.matches.map((match) => <li key={match.date.day}>{match.date.day}</li>)}
-              </List.Item>
-              <List.Item>
-                <List.Header>Time of event</List.Header>
-                {this.state.matches.map((match) => <li key={match.date.hour}>{match.date.hour}</li>)}
-              </List.Item>
-            </List>
-          </Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment>
-            <List>
-              <List.Item>
-                <List.Header>Host</List.Header>
-                {this.state.user.name}
-              </List.Item>
-              <List.Item>
-                <List.Header>Contestants:</List.Header>
-                <ul>
-                  {this.state.players.map((player) => <li key={player.name}>{player.name}</li>)}
-                </ul>
-              </List.Item>
-            </List>
-          </Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment>
-            <List.Header>Info:</List.Header>
-            {this.state.matches.map((match) => <p key={match.comment}>{match.comment}</p>)}
-          </Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment>
-            <div>Google MAP</div>
-          </Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment>
-            <button onClick={this.AddPlayerToEvent}>Join event</button>
-          </Segment>
-        </Grid.Column>
-      </Grid>
+      <div className='sport-details' style={{ display: 'none' }}>
+        <Grid stackable columns={2}>
+          <Grid.Column>
+            <Segment>
+              <List>
+                <List.Item>
+                  <List.Header>Sport</List.Header>{}
+                </List.Item>
+                <List.Item>
+                  <List.Header>Adress</List.Header>
+                  {this.props.clickedMatch.map((match) => <li key={match.localization.street}>{this.props.localization.city}</li>)}
+                </List.Item>
+                <List.Item>
+                  <List.Header>Date of event</List.Header>
+                  {this.props.matches.map((match) => <li key={match.date.day}>{this.props.date.day}</li>)}
+                </List.Item>
+                <List.Item>
+                  <List.Header>Time of event</List.Header>
+                  {this.props.matches.map((match) => <li key={match.date.hour}>{this.props.date.hour}</li>)}
+                </List.Item>
+              </List>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>
+              <List>
+                <List.Item>
+                  <List.Header>Host</List.Header>
+                  {this.props.user.name}
+                </List.Item>
+                <List.Item>
+                  <List.Header>Contestants:</List.Header>
+                  <ul>
+                    {this.state.players.map((player) => <li key={player.name}>{player.name}</li>)}
+                  </ul>
+                </List.Item>
+              </List>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>
+              <List.Header>Info:</List.Header>
+              {this.state.matches.map((match) => <p key={match.comment}>{match.comment}</p>)}
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>
+              <div>Google MAP</div>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <Segment>
+              <button onClick={this.AddPlayerToEvent}>Join event</button>
+            </Segment>
+          </Grid.Column>
+        </Grid>
+      </div>
     );
   }
 }
--- src/components/matches/Matches.js
+++ src/components/matches/Matches.js
@@ -4,6 +4,7 @@ import { Table, Icon } from 'semantic-ui-react'
 import MatchesForm from './MatchesForm'
 
 import './Matches.css'
+import SportDetails from "../Sport-details/Sport-details";
 
 
 const fetchMatches = async () => {
@@ -18,24 +19,40 @@ const fetchSports = async () => {
     return sports;
 }
 
+const fetchPlayers = async () => {
+    const response = await fetch(process.env.PUBLIC_URL + "/players.json");
+    const players = await response.json();
+    return players;
+}
+
+const fetchUser = async () => {
+    const response = await fetch(process.env.PUBLIC_URL + '/user.json')
+    const user = await response.json()
+    return user
+}
 
 class Matches extends Component {
 
     state = {
         matches: [],
         sports: [],
+        players: [],
+        user: [],
         filter: {
             location: '',
             sport: '',
         },
         filterVisible: false,
+        clickedMatch: {}
     };
 
     componentDidMount() {
-        Promise.all([fetchMatches(), fetchSports()])
-            .then(([matches, sports]) => this.setState({
+        Promise.all([fetchMatches(), fetchSports(), fetchPlayers(), fetchUser()])
+            .then(([matches, sports, players, user]) => this.setState({
                 matches: matches,
                 sports: sports,
+                players: players,
+                user: user
             }))
     }
 
@@ -44,7 +61,7 @@ class Matches extends Component {
     setNewFilter = () => {
         const sport = document.querySelector('#matches-sports-select').childNodes[1].innerText !== 'Sport' ? document.querySelector('#matches-sports-select').childNodes[1].innerText : ''
         const location = document.querySelector('#form-input-control-location-matches').value
-        
+
         this.setState({
             filter: {
                 location: location,
@@ -58,86 +75,117 @@ class Matches extends Component {
         this.state.filterVisible ? this.setState({ filterVisible: false }) : this.setState({ filterVisible: true })
     }
 
+    saveClickedMatchInfo = (match) => {
+        this.setState({
+            clickedMatch: this.state.matches.find(chosenMatch => chosenMatch.date === match.date)
+        })
+    }
+
+    handleMatchClick = (e) => {
+        const match = document.querySelector('.sport-details')
+        const matchList = document.querySelector('.matches')
+
+        if (match.style.display === 'none') {
+            this.saveClickedMatchInfo(e)
+            match.style.display = 'flex'
+            matchList.style.display = 'none'
+        } else {
+            match.style.display = 'none'
+            matchList.style.display = 'block'
+        }
+    }
+
 
     render() {
 
         return (
-            <div>
-
-                <MatchesForm
-                    toggleFilter={this.toggleFilter}
-                    filterStatus={this.state.filterVisible}
-                    searchForMatch={this.setNewFilter}
-                    sports={this.state.sports.map(
-                        sport => ({
-                            key: sport.id,
-                            text: sport.name,
-                            value: sport.name,
-                        })
-                    )}
+            <div className="componentWrapper">
+                <SportDetails
+                    toggleMatchView={this.handleMatchClick}
+                    matches={this.state.clickedMatch}
+                    players={this.state.players}
+                    sports={this.state.sports}
+                    user={this.state.user}
                 />
-                <Table celled striped>
+
+                <div className="matches">
+                    <MatchesForm
+                        toggleFilter={this.toggleFilter}
+                        filterStatus={this.state.filterVisible}
+                        searchForMatch={this.setNewFilter}
+                        sports={this.state.sports.map(
+                            sport => ({
+                                key: sport.id,
+                                text: sport.name,
+                                value: sport.name,
+                            })
+                        )}
+                    />
+                    <Table celled striped>
 
 
-                    <Table.Body className="matches">
-                        <Table.Header>
-                            <Table.Row key="matches-header">
-                                <Table.HeaderCell>
-                                    <Table.Cell >LOCATION</Table.Cell>
-                                    <Table.Cell >SPORT</Table.Cell>
-                                    <Table.Cell >DATE</Table.Cell>
-                                    <Table.Cell >PLAYERS</Table.Cell>
-                                </Table.HeaderCell>
-                            </Table.Row>
-                        </Table.Header>
-                        {
-                            this.state.matches
-                                .filter(
-                                    match => (
-                                        match.localization.city.toLowerCase().includes(this.state.filter.location.toLowerCase())
-                                    ))
-                                .filter(
-                                    match => (
-                                        this.state.sports
-                                            .filter(sport => match.sportID === sport.id)
-                                            .map(sport => sport.name)
-                                            .concat('')
-                                    ).includes(this.state.filter.sport))
-                                .map(
-                                    match => (
-                                        <Table.Row key={match.id}>
-
-                                            <Table.Cell>
-                                                <Table.Cell>{match.localization.city}
-                                                </Table.Cell>
-
-                                                <Table.Cell>{
-                                                    this.state.sports
-                                                        .filter(sport => match.sportID === sport.id)
-                                                        .map(sport => `${sport.name.charAt(0).toUpperCase() + sport.name.slice(1)}`)
-                                                }</Table.Cell>
+                        <Table.Body className="matches">
+                            <Table.Header>
+                                <Table.Row key="matches-header">
+                                    <Table.HeaderCell>
+                                        <Table.Cell >LOCATION</Table.Cell>
+                                        <Table.Cell >SPORT</Table.Cell>
+                                        <Table.Cell >DATE</Table.Cell>
+                                        <Table.Cell >PLAYERS</Table.Cell>
+                                    </Table.HeaderCell>
+                                </Table.Row>
+                            </Table.Header>
+                            {
+                                this.state.matches
+                                    .filter(
+                                        match => (
+                                            match.localization.city.toLowerCase().includes(this.state.filter.location.toLowerCase())
+                                        ))
+                                    .filter(
+                                        match => (
+                                            this.state.sports
+                                                .filter(sport => match.sportID === sport.id)
+                                                .map(sport => sport.name)
+                                                .concat('')
+                                        ).includes(this.state.filter.sport))
+                                    .map(
+                                        match => (
+                                            <Table.Row key={match.id}>
 
                                                 <Table.Cell>
-                                                    {match.date.day}
+                                                    <Table.Cell>{match.localization.city}
+                                                    </Table.Cell>
+
+                                                    <Table.Cell>{
+                                                        this.state.sports
+                                                            .filter(sport => match.sportID === sport.id)
+                                                            .map(sport => `${sport.name.charAt(0).toUpperCase() + sport.name.slice(1)}`)
+                                                    }</Table.Cell>
+
+                                                    <Table.Cell>
+                                                        {match.date.day}
+                                                    </Table.Cell>
+
+                                                    <Table.Cell>
+                                                        {match.playerIDs.map(
+                                                            player => (
+                                                                <Icon name="user" size="small"></Icon>
+                                                            ))}
+                                                    </Table.Cell>
                                                 </Table.Cell>
 
-                                                <Table.Cell>
-                                                    {match.playerIDs.map(
-                                                        player => (
-                                                            <Icon name="user" size="small"></Icon>
-                                                        ))}
-                                                </Table.Cell>
-                                            </Table.Cell>
+                                            </Table.Row>
+                                        )
+                                    )}
 
-                                        </Table.Row>
-                                    )
-                                )}
-
-                    </Table.Body>
-                </Table>
+                        </Table.Body>
+                    </Table>
+                </div>
             </div>
         )
+
     }
+
 }
 
 export default Matches
