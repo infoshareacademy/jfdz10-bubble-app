021ceb443ee6b6142fba02a96c42f6392e68afa8
mik5owski <piatkowskimikolaj@gmail.com>
2019-03-26 19:04:39 +0100

    added special styling to active menu items

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -7,43 +7,28 @@ import './Navigation.css'
 
 class Navigation extends Component {
 
-    changeColor = (e) => {
-        e.stopPropagation()
-        const listItems = document.querySelectorAll('.ListItem')
-        const navImages = document.querySelectorAll('.NavImage')
-        const navLabels = document.querySelectorAll('.NavLabel')
-        console.log(e, listItems)
-        const listItem = e.target
-
-        listItems.forEach(item => item.classList.remove('ActiveNav'))
-        navImages.forEach(item => item.classList.remove('ActiveNav'))
-        navLabels.forEach(item => item.classList.remove('ActiveNav'))
-        listItem.className += ' ActiveNav'
-
-    }
-
     render() {
         
         return (
         <div className="Navigation">
             <ul className="ListContainer">
-                <li class='lol' className="ListItem" onClick={(e) => this.changeColor(e)}><NavLink to="/">
+                <li class='lol' className="ListItem"><NavLink exact activeClassName='active' to="/">
                     <image className='NavImage Home'></image>
                     <label className='NavLabel'>Home</label>
                 </NavLink></li>
-                <li class='lol' className="ListItem"onClick={(e) => this.changeColor(e)}><NavLink to="/addMatch">
+                <li class='lol' className="ListItem"><NavLink activeClassName='active' to="/addMatch">
                     <image className='NavImage AddMatch'></image>
                     <label className='NavLabel'>AddMatch</label>
                 </NavLink></li>
-                <li className="ListItem"onClick={(e) => this.changeColor(e)}><NavLink to="/players">
+                <li className="ListItem"><NavLink activeClassName='active' to="/players">
                     <image className='NavImage Players'></image>
                     <label className='NavLabel'>Players</label>
                 </NavLink></li>
-                <li className="ListItem"onClick={(e) => this.changeColor(e)}><NavLink to="/matches">
+                <li className="ListItem"><NavLink activeClassName='active' to="/matches">
                     <image className='NavImage Matches'></image>
                     <label className='NavLabel'>Matches</label>
                 </NavLink></li>
-                <li className="ListItem"onClick={(e) => this.changeColor(e)}><NavLink to="/profile">
+                <li className="ListItem"><NavLink activeClassName='active' to="/profile">
                     <image className='NavImage NavProfile'></image>
                     <label className='NavLabel'>Profile</label>
                 </NavLink></li>
