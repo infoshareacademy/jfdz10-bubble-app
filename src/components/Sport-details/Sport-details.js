bdba2d6a7fb7357c796ba27699ba3e30877c942e
WitoG <wit1godlewski@gmail.com>
2019-05-11 13:53:56 +0200

    -

--- src/components/Sport-details/Sport-details.js
+++ src/components/Sport-details/Sport-details.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import { Grid, Segment } from "semantic-ui-react";
 import { List } from "semantic-ui-react";
+import "./Sport-details.css";
 
 class SportDetails extends Component {
+
+  AddPlayerToEvent = (e) => {
+    const userName = this.props.user.name;
+
+  }
+
   render() {
 
-    console.log(this.props);
+    // console.log(this.props);
     const playerIDs = this.props.match.playerIDs;
     const sportID = this.props.match.sportID;
     let matchPlayers;
@@ -16,36 +23,35 @@ class SportDetails extends Component {
         playerIDs.includes(player.id)
       );
     }
-    ////////////////Tutaj nie działa
+
     if (sportID) {
       sportName = this.props.sports.filter(sport =>
-        sportID.includes(sport.id)
-      );
+        sportID === sport.id)
     }
-    ///////////////////////
+
     return (
       <div className="sport-details" style={{ display: "none" }}>
         <Grid stackable columns={2}>
           <Grid.Column>
             <button
-              className="toggle-view-button"
+              id="button"
               onClick={() => {
                 this.props.toggleMatchView();
               }}
             >
-              X
+              Back
             </button>
             <Segment>
               <List>
                 <List.Item>
-                  <List.Header>Sport</List.Header>
+                  <List.Header className='header'>Sport</List.Header>
                   {sportName &&
                     sportName.map(sport => {
                       return <li key={sport.id}>{sport.name}</li>;
                     })}
                 </List.Item>
                 <List.Item>
-                  <List.Header>Address</List.Header>
+                  <List.Header className='header'>Address</List.Header>
                   <li>
                     {this.props.match.localization
                       ? this.props.match.localization.city
@@ -53,13 +59,13 @@ class SportDetails extends Component {
                   </li>
                 </List.Item>
                 <List.Item>
-                  <List.Header>Date of event</List.Header>
+                  <List.Header className='header'>Date of event</List.Header>
                   <li>
                     {this.props.match.date ? this.props.match.date.day : null}
                   </li>
                 </List.Item>
                 <List.Item>
-                  <List.Header>Time of event</List.Header>
+                  <List.Header className='header'>Time of event</List.Header>
                   <li>
                     {this.props.match.date ? this.props.match.date.hour : null}
                   </li>
@@ -71,15 +77,15 @@ class SportDetails extends Component {
             <Segment>
               <List>
                 <List.Item>
-                  <List.Header>Host</List.Header>
-                  {this.props.user.name}
+                  <List.Header className='header'>Host</List.Header>
+                  <li id='player-info'>{this.props.user.name}</li>
                 </List.Item>
                 <List.Item>
-                  <List.Header>Players:</List.Header>
+                  <List.Header className='header'>Players</List.Header>
                   <ul>
                     {matchPlayers &&
                       matchPlayers.map(player => {
-                        return <li key={player.id}>{player.name}</li>;
+                        return <li id='player-info' key={player.id}>{player.name}</li>;
                       })}
                   </ul>
                 </List.Item>
@@ -88,19 +94,16 @@ class SportDetails extends Component {
           </Grid.Column>
           <Grid.Column>
             <Segment>
-              <List.Header>Host comment:</List.Header>
-              <p key={this.props.match.comment}>{this.props.match.comment}</p>
+              <List>
+                <List.Item>
+                  <List.Header className='header'>Host comment</List.Header>
+                  <p key={this.props.match.comment}>{this.props.match.comment}</p>
+                </List.Item>
+              </List>
             </Segment>
           </Grid.Column>
           <Grid.Column>
-            <Segment>
-              <div>Google MAP</div>
-            </Segment>
-          </Grid.Column>
-          <Grid.Column>
-            <Segment>
-              <button onClick={this.AddPlayerToEvent}>Join event</button>
-            </Segment>
+            <button id='button' onClick={this.AddPlayerToEvent}>Join event</button>
           </Grid.Column>
         </Grid>
       </div>
