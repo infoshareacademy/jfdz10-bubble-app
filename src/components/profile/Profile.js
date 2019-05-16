48dfe0822813dcffc260635e8aa5a0b5a9c8c983
mik5owski <piatkowskimikolaj@gmail.com>
2019-05-16 13:46:29 +0200

    fixed favplayers in profile and added signing up option to profile page when user is not logged in

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
+import { Button, Form, Input } from 'semantic-ui-react'
+
 import './Profile.css'
 
 const fetchUser = async () => {
@@ -15,7 +17,13 @@ class Profile extends Component {
         sports: [],
         players: [],
         refs: [],
-        ref: ''
+        ref: '',
+        isSigningIn: false,
+        isSigningUp: false,
+        email: '',
+        password: '',
+        name: '',
+        city: ''
     }
 
     componentDidMount() {
@@ -23,18 +31,22 @@ class Profile extends Component {
         this.getPlayers()
 
         const ref = firebase.auth().onAuthStateChanged(user =>
-            user ? this.setState({
-                loggedInUserID: user.uid
-            }, this.state.loggedInUserID ? () => firebase.database().ref('players/' + this.state.loggedInUserID).once('value')
-            .then(snapshot => this.setState({
+            user 
+            ? this.setState({
                 ...this.state,
-                user: snapshot.val()
-            })) : () => (console.log('hello')))
+                loggedInUserID: user.uid
+                }, () => firebase.database().ref('players/' + this.state.loggedInUserID).once('value')
+                    .then(snapshot => this.setState({
+                        ...this.state,
+                        user: snapshot.val()
+                    }))
+            )
             : this.setState({
+                ...this.state,
                 loggedInUserID: null
             })
         )
-        
+
         this.setState({
             ref
         })
@@ -84,6 +96,77 @@ class Profile extends Component {
         firebase.auth().signOut()
             .then( alert('Succesfully signed out.'))
             .catch(error => alert(error.message))
+    }
+
+    handleSignIn = () => {
+        this.setState({
+            isSigningIn: true,
+            isSigningUp: false
+        })
+    }
+
+    handleSignUp = () => {
+        this.setState({
+            isSigningUp: true,
+            isSigningIn: false
+        })
+    }
+
+    handleChange = (event, { name, value }) => {
+        if (this.state.hasOwnProperty(name)) {
+            this.setState({ [name]: value });
+        }
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        if(this.state.isSigningUp) {
+            firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+                .then(() => {
+                    alert('Registration has succeded. Welcome to the game!');
+                    this.setState({
+                        isSigningIn: false,
+                        isSigningUp: false
+                    })
+                })
+                .then(
+                    setTimeout(this.addNewUserToDatabase, 3000)
+                )
+                .catch((error) => {alert(error.message)})
+            
+        } else {
+            firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+                .then(() => {
+                    alert('You have succesfully logged in. Welcome back!')
+                    this.setState({
+                        isSigningIn: false,
+                        isSigningUp: false
+                    })
+                })
+                .catch((error) => {alert(error.message)})
+        }
+    };
+
+    signInWithGoogle = () => {
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+            var user = result.user;
+            if(!firebase.database().ref('players/' + user.uid)) {
+            firebase.database().ref('players/' + user.uid).set(
+                {
+                avatar: user.photoURL,
+                dateOfJoining: user.metadata.creationTime,
+                eMail: user.email,
+                id: user.uid,
+                localization: "Unknown",
+                name: user.displayName
+                } 
+            )
+          }}).catch(function(error) {
+            var errorMessage = error.message;
+            alert(errorMessage)
+          });
     }
 
     render() {
@@ -162,14 +245,76 @@ class Profile extends Component {
                     <ol className="FavouriteSportsList">
                         {this.state.players
                             // .filter(player => player.id === this.state.user.favouritePlayersIDs.find(id => id === player.id) || [])
-                            .filter(player => favPlayers.includes(player.id) || [])
+                            .filter(player => (favPlayers || []).includes(player.id))
                             .map(player => (
                                 <li className="FavouriteSportsListItem" key={player.id}>{player.name}</li>
                             ))}
                     </ol>
                     </div>
                 </div> )
-                     : (<h1>Please Sign In to see Your profile details.</h1>)
+                     : (<div>
+                            <div 
+                            className="signing"
+                            style={{display: this.state.loggedInUserID ? 'none' : 'flex'}}
+                            >
+                                <button class="ui primary button" onClick={this.handleSignIn}>
+                                    Sign In
+                                </button>
+                                <button class="ui button" onClick={this.handleSignUp}>
+                                    Sign Up
+                                </button>
+                                <button class="ui button" onClick={this.signInWithGoogle}>
+                                    Sign In With Google
+                                </button>
+                            </div>
+                            <div className="signing-in">
+                            <Form
+                                className="add-a-match-form"
+                                onSubmit={this.handleSubmit}
+                                style={{display: this.state.isSigningIn || this.state.isSigningUp ? 'block' : 'none'}}
+                            >
+            
+                                <Form.Group widths='equal'>
+                                    <Form.Field
+                                        name='email'
+                                        control={Input}
+                                        label='Email'
+                                        placeholder='john.bull@email.com'
+                                        onChange={this.handleChange} />
+                                    <Form.Field
+                                        name='password'
+                                        control={Input}
+                                        label='Password'
+                                        placeholder='Password'
+                                        onChange={this.handleChange} />
+                                    <Form.Field
+                                        name='name'
+                                        control={Input}
+                                        label={this.state.isSigningUp ? 'Name' : ''}
+                                        placeholder='John'
+                                        onChange={this.handleChange}
+                                        style={{display: this.state.isSigningUp ? 'flex' : 'none'}} />
+                                <Form.Field
+                                        name='city'
+                                        control={Input}
+                                        label={this.state.isSigningUp ? 'City' : ''}
+                                        placeholder='SimCity'
+                                        onChange={this.handleChange}
+                                        style={{display: this.state.isSigningUp ? 'flex' : 'none'}} />
+                                </Form.Group>
+            
+                                <Form.Field
+                                    disabled={
+                                        !this.state.email
+                                        || !this.state.password
+                                        || (this.state.isSigningUp && !this.state.name)
+                                        || (this.state.isSigningUp && !this.state.city)
+                                    }
+                                    control={Button}>{this.state.isSigningIn ? "Sign in" : "Sign Up"}
+                                </Form.Field>
+                            </Form>
+                            </div>
+                        </div>)
                 }
             </div>
         )
