66381a888fec31f9be56c2c03abbe2bc0f0e72ff
crchs <kolenycz@gmail.com>
2019-05-19 15:11:12 +0200

    removed test.js

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -4,11 +4,7 @@ import { Button, Form, Input } from 'semantic-ui-react'
 
 import './Profile.css'
 
-const fetchUser = async () => {
-    const response = await fetch(process.env.PUBLIC_URL + '/user.json')
-    const user = await response.json()
-    return user
-}
+
 
 class Profile extends Component {
     state = {
@@ -52,9 +48,6 @@ class Profile extends Component {
         })
 
     }
-    
-    
-
      
     componentWillUnmount() {
         this.state.ref && this.state.ref();
